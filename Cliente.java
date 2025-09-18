423b9f1ea56978cdb57d75895e4fe653d38057e8
Enzo Libório <enzoliborioo@gmail.com>
2025-09-18 19:01:23 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,10 @@
+public class Cliente {
+    private String nome;
+    private String documento;
+    private int alugou;
+
+    public Cliente(String nome, String documento) {
+        this.nome = nome;
+        this.documento = documento;
+    }
+}
